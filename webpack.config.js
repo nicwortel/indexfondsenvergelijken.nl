9b8d85359d7b05fbb693b8cd3cfae820a3764a75
Nic Wortel <nic@nicwortel.nl>
2020-06-02 01:49:13 +0200

    Use cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.ts',
@@ -16,15 +18,22 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: 'index.html',
+            minify: false
+        }),
         new CopyPlugin({
             patterns: [
-                { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: '' },
-                { from: 'node_modules/bootstrap-icons/icons/question-circle.svg', to: 'icons/' },
-                { from: 'node_modules/bootstrap-icons/icons/info-circle.svg', to: 'icons/' },
+                {from: 'assets/main.css', to: ''},
+                {from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: ''},
+                {from: 'node_modules/bootstrap-icons/icons/question-circle.svg', to: 'icons/'},
+                {from: 'node_modules/bootstrap-icons/icons/info-circle.svg', to: 'icons/'},
             ],
         }),
     ],
