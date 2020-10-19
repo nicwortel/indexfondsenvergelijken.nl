a0d05c11f509f9e51f63e1c08bf7215a48f8305d
Nic Wortel <nic@nicwortel.nl>
2020-10-19 15:19:49 +0200

    Display when a portfolio contains only emerging markets small caps

--- src/Portfolio.ts
+++ src/Portfolio.ts
@@ -5,7 +5,10 @@ import {Transaction} from "./Transaction";
 
 export class Portfolio {
     constructor(public assets: { allocation: Percentage, fund: Fund }[]) {
-        const totalAllocation = assets.reduce((sum: Percentage, current: { allocation: Percentage; fund: Fund }) => sum.add(current.allocation), new Percentage(0));
+        const totalAllocation = assets.reduce((
+            sum: Percentage,
+            current: { allocation: Percentage; fund: Fund }
+        ) => sum.add(current.allocation), new Percentage(0));
 
         if (!totalAllocation.equals(new Percentage(100))) {
             throw new Error('The total allocation of all assets should be 100%');
@@ -20,26 +23,21 @@ export class Portfolio {
     }
 
     public getTotalCosts(): Percentage {
-        const costs = this.assets.map((asset: { allocation: Percentage; fund: Fund }): Percentage => asset.allocation.multiply(asset.fund.getTotalRecurringCosts()));
+        const costs = this.assets.map((asset: { allocation: Percentage; fund: Fund }): Percentage => asset.allocation.multiply(
+            asset.fund.getTotalRecurringCosts()));
 
         return costs.reduce((sum: Percentage, current: Percentage) => sum.add(current));
     }
 
     public getMarketCapPercentage(): Percentage {
-        return this.assets.reduce((
+        return this.getFunds().reduce((
             previous: Percentage,
-            current: { allocation: Percentage; fund: Fund }
-        ) => previous.add(current.fund.getMarketCapitalization()), new Percentage(0));
+            current: Fund
+        ) => previous.add(current.getMarketCapitalization()), new Percentage(0));
     }
 
     public getFundNames(): string[] {
-        return this.assets.map(function (asset): string {
-            return asset.fund.getIdentifier();
-        });
-    }
-
-    public getNumberOfShares(): number {
-        return this.assets.reduce((sum: number, current) => sum + current.fund.getNumberOfShares(), 0);
+        return this.getFunds().map((fund: Fund) => fund.getIdentifier());
     }
 
     public containsSmallCaps(): boolean {
@@ -52,8 +50,18 @@ export class Portfolio {
         return false;
     }
 
+    public containsOnlyEmergingMarketsSmallCaps(): boolean {
+        const smallCapFunds = this.getFunds().filter((fund: Fund) => fund.containsSmallCaps());
+        const developedSmallCapFunds = smallCapFunds.filter((fund: Fund) => fund.getTrackedIndex().markets === 'all-world' || fund.getTrackedIndex().markets === 'developed');
+
+        return developedSmallCapFunds.length === 0;
+    }
+
     public allocate(investment: Money): Transaction[] {
-        return this.assets.map((asset: { allocation: Percentage; fund: Fund }) => new Transaction(asset.fund, asset.allocation.applyTo(investment)));
+        return this.assets.map((asset: { allocation: Percentage; fund: Fund }) => new Transaction(
+            asset.fund,
+            asset.allocation.applyTo(investment)
+        ));
     }
 
     public describe(): string {
@@ -91,5 +99,9 @@ export class Portfolio {
         }
 
         return markets.filter(onlyUnique).join(' ') + ' ' + sizes.filter(onlyUnique).join(' & ') + ' cap' + weighting;
+    }
+
+    private getFunds(): Fund[] {
+        return this.assets.map((asset: { allocation: Percentage; fund: Fund }) => asset.fund);
     }
 }
