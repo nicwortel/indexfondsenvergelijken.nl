904b5863aeb73732aa48c93bee979d14103b4392
Nic Wortel <nic@nicwortel.nl>
2023-07-10 16:19:48 +0200

    Remove old Matomo tracking code

--- src/index.ts
+++ src/index.ts
@@ -96,23 +96,3 @@ if (form) {
         runSimulation(portfolios);
     };
 }
-
-declare global {
-    interface Window {
-        _paq: any;
-    }
-}
-
-let _paq = window._paq = window._paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function () {
-    var u = "https://stats.nicwortel.nl/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', 2]);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript';
-    g.async = true;
-    g.src = u + 'matomo.js';
-    s.parentNode.insertBefore(g, s);
-})();
